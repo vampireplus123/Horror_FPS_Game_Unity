cbbd07495a3f06314245d0d7a8279fdd21ae2b32
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-05-11 18:18:00 +0700

    character movement

--- Assets/Scripts/Player/ExternalACtions.cs
+++ Assets/Scripts/Player/ExternalACtions.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour
+public class ExternalACtions : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
